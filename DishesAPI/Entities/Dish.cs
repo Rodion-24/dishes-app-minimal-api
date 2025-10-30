ed16c4882526e72e8e6e42ce220e42c7689d720f
Rodion-24 <your-email@example.com>
2025-10-30 22:56:46 +0100

    init project

---
+++ DishesAPI/Entities/Dish.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DishesAPI.Entities;
+
+public class Dish
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    // "required" modifier: compiler guarantees Name is initialized when the Dish is instantiated.
+    [Required]
+    [MaxLength(200)]
+    public required string Name { get; set; }
+
+    public ICollection<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
+
+    public Dish()
+    {
+    }
+
+    [SetsRequiredMembers]
+    public Dish(Guid id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
